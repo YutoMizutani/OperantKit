752b7ef9d626d1886ec77ac46384fa8bb0e8e59e
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-08-19 18:04:43 +0900

    :file_folder: Add targets for iOS and macOS

---
+++ OperantKit macOS/OperantKit_macOS.h
@@ -0,0 +1,18 @@
+//
+//  OperantKit_macOS.h
+//  OperantKit macOS
+//
+//  Created by Yuto Mizutani on 2018/08/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OperantKit_macOS.
+FOUNDATION_EXPORT double OperantKit_macOSVersionNumber;
+
+//! Project version string for OperantKit_macOS.
+FOUNDATION_EXPORT const unsigned char OperantKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OperantKit_macOS/PublicHeader.h>
+
+
