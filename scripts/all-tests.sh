2e604e078b964663b554c3c317d2f73fdf1f10d7
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-08-19 18:35:38 +0900

    :file_folder: Add Travis CI settings

---
+++ scripts/all-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$1" == "iOS" ]; then
+	set -o pipefail && xcodebuild test -verbose -scheme "OperantKit iOS" -destination "platform=iOS Simulator,OS=11.4,name=iPhone 8 Plus" ONLY_ACTIVE_ARCH=NO | xcpretty
+elif [ "$1" == "macOS" ]; then
+	set -o pipefail && xcodebuild test -verbose -scheme "OperantKit macOS" ONLY_ACTIVE_ARCH=NO | xcpretty
+else
+	echo "wrong parameters. (iOS|macOS)"
+	exit 1
+fi
