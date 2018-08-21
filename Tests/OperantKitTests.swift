106a1ca5eb55668e95eeb13516a02c86375a8cbe
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-08-21 10:24:57 +0900

    :apple: Red test

--- Tests/OperantKitTests.swift
+++ Tests/OperantKitTests.swift
@@ -6,7 +6,7 @@ final class OperantKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(OperantKit().text, "Hello, World!")
+        XCTAssertEqual(OperantKit().text, "Hello, World!!")
     }
 
 
