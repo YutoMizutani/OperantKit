8d104d2cf0a85ce602e5757d5c5d3b07ceb28682
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-22 05:04:39 +0900

    :sparkles: Add view

---
+++ Demo/OperantApp/OperantApp/Presentation/View/RatChamber/RatChamberView.swift
@@ -0,0 +1,17 @@
+//
+//  RatChamberView.swift
+//  OperantApp
+//
+//  Created by Yuto Mizutani on 2018/10/22.
+//  Copyright © 2018 Yuto Mizutani. All rights reserved.
+//
+
+import UIKit
+
+class RatChamberView: UIViewController {
+    @IBOutlet weak var baseView: RatChamberBaseView!
+    @IBOutlet weak var leftLight: RatChamberLightView!
+    @IBOutlet weak var rightLight: RatChamberLightView!
+    @IBOutlet weak var leftLever: RatChamberLeverButton!
+    @IBOutlet weak var rightLever: RatChamberLeverButton!
+}
