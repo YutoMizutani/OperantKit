e6170a89acbd34971377acf3c121458f727081f4
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-22 01:32:34 +0900

    :tada: Add clean directory

--- Demo/OperantApp/OperantApp/Application/AppDelegate/AppDelegate.swift
+++ Demo/OperantApp/OperantApp/Application/AppDelegate/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = MainBuilder().build()
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
--- Demo/OperantApp/OperantApp/Presentation/View/ViewController.swift
+++ Demo/OperantApp/OperantApp/Presentation/View/ViewController.swift
@@ -2,19 +2,15 @@
 //  ViewController.swift
 //  OperantApp
 //
-//  Created by Yuto Mizutani on 2018/10/22.
+//  Created by Yuto Mizutani on 2018/10/19.
 //  Copyright © 2018 Yuto Mizutani. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
