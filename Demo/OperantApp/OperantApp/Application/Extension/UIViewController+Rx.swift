c9e838693d68897d97f45dbba2b1b611789c6495
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-28 11:15:08 +0900

    :+1: Update safe to call about UIViewController

--- Demo/OperantApp/OperantApp/Application/Extension/UIViewController+Rx.swift
+++ Demo/OperantApp/OperantApp/Application/Extension/UIViewController+Rx.swift
@@ -12,22 +12,22 @@ import UIKit
 
 extension Reactive where Base: UIViewController {
     var viewDidLoad: Observable<[Any]> {
-        return self.sentMessage(#selector(self.base.viewDidLoad)).share(replay: 1)
+        return self.methodInvoked(#selector(self.base.viewDidLoad)).share(replay: 1)
     }
 
     var viewWillAppear: Observable<[Any]> {
-        return self.sentMessage(#selector(self.base.viewWillAppear)).share(replay: 1)
+        return self.methodInvoked(#selector(self.base.viewWillAppear)).share(replay: 1)
     }
 
     var viewDidAppear: Observable<[Any]> {
-        return self.sentMessage(#selector(self.base.viewDidAppear)).share(replay: 1)
+        return self.methodInvoked(#selector(self.base.viewDidAppear)).share(replay: 1)
     }
 
     var viewWillDisappear: Observable<[Any]> {
-        return self.sentMessage(#selector(self.base.viewWillDisappear)).share(replay: 1)
+        return self.methodInvoked(#selector(self.base.viewWillDisappear)).share(replay: 1)
     }
 
     var viewDidDisappear: Observable<[Any]> {
-        return self.sentMessage(#selector(self.base.viewDidDisappear)).share(replay: 1)
+        return self.methodInvoked(#selector(self.base.viewDidDisappear)).share(replay: 1)
     }
 }
