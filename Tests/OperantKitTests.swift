6da077c0f79e9de3eba3bf71c6e893a2ca6f381f
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-30 13:55:24 +0900

    :+1: Fix build #38

--- Tests/OperantKitTests.swift
+++ Tests/OperantKitTests.swift
@@ -12,63 +12,7 @@ final class OperantKitTests: XCTestCase {
         let timer = IntervalTimer(0.1)
         timer.start()
         usleep(UInt32(targetMilliseconds) * 1000)
-        XCTAssertGreaterThanOrEqual(timer.elapsed.milliseconds.value, targetMilliseconds - self.toleranceDelay)
+        XCTAssertGreaterThanOrEqual(timer.milliseconds, targetMilliseconds - self.toleranceDelay)
         timer.finish()
-    }
-
-    /// Random 10 times call test
-    func testEvent() {
-        #if DEBUG
-        let disposeBag = DisposeBag()
-        let targetSeconds: Int = 1
-        let timer = IntervalTimer(0.1, isDebug: true)
-        var randomNumber: Int { return Int(arc4random() % UInt32(targetSeconds * 1000)) }
-        let randomTimes: [Int] = [
-            // 10 random events
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-            randomNumber,
-        ].sorted()
-        randomTimes.enumerated().forEach { timer.addAlerm((id: $0.offset, milliseconds: $0.element)) }
-        timer.eventFlag.asObservable()
-            .filter { $0 != nil }.map { $0! }
-            .subscribe(onNext: { event in
-                // Decision equal event time and ids
-                XCTAssertEqual(event.milliseconds, randomTimes[event.id])
-                // Decision equal event time and current elapsed time
-                XCTAssertGreaterThanOrEqual(timer.elapsed.milliseconds.value, event.milliseconds)
-            })
-            .disposed(by: disposeBag)
-        timer.start()
-        sleep(UInt32(targetSeconds))
-        XCTAssertGreaterThanOrEqual(timer.elapsed.milliseconds.value, targetSeconds * 1000 - self.toleranceDelay)
-        timer.finish()
-        #endif
-    }
-
-    /// Test DEBUG print for great developers
-    func testPrint() {
-        #if DEBUG
-        let disposeBag = DisposeBag()
-        let targetSeconds: Int = 5
-        let timer = IntervalTimer(0.1, isDebug: true)
-        timer.debugText.asObservable()
-            .filter { $0 != nil }.map { $0! }
-            .subscribe(onNext: { text in
-                print(text.split(separator: "\n").map { "### \($0)" }.joined(separator: "\n"), terminator: "\n\n")
-            })
-            .disposed(by: disposeBag)
-        timer.start()
-        sleep(UInt32(targetSeconds))
-        XCTAssertGreaterThanOrEqual(timer.elapsed.milliseconds.value, targetSeconds * 1000 - self.toleranceDelay)
-        timer.finish()
-        #endif
     }
 }
