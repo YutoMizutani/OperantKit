c7b9e83c91dcb4ecee729baa6ea5c5a9c6449833
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-11-13 11:47:11 +0900

    :sparkles: Add Time schedule logic #102

---
+++ Sources/Common/Schedules/RT.swift
@@ -0,0 +1,22 @@
+//
+//  RT.swift
+//  OperantKit macOS
+//
+//  Created by Yuto Mizutani on 2018/11/13.
+//
+
+import RxSwift
+
+extension Observable where E == ResponseEntity {
+
+    /// Random time schedule
+    public func RT(_ value: Int, with entities: E...) -> Observable<ReinforcementResult> {
+        return self
+            .randomTime(value, entities)
+    }
+
+    /// RT logic
+    func randomTime(_ value: Int, _ entities: [E]) -> Observable<ReinforcementResult> {
+        return self.fixedTime(value, entities)
+    }
+}
---
+++ Sources/Common/Schedules/VT.swift
@@ -0,0 +1,22 @@
+//
+//  VT.swift
+//  OperantKit macOS
+//
+//  Created by Yuto Mizutani on 2018/11/13.
+//
+
+import RxSwift
+
+extension Observable where E == ResponseEntity {
+
+    /// Variable time schedule
+    public func VT(_ value: Int, with entities: E...) -> Observable<ReinforcementResult> {
+        return self
+            .variableTime(value, entities)
+    }
+
+    /// VT logic
+    func variableTime(_ value: Int, _ entities: [E]) -> Observable<ReinforcementResult> {
+        return self.fixedTime(value, entities)
+    }
+}
