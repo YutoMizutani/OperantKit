ffbb6e56264cf6d026b82cbb2e9a7422e4ce93c8
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-11-26 16:58:10 +0900

    :apple: Add UI test #122

--- Examples/iOS/RatChamber/RatChamberUITests/RatChamberUITests.swift
+++ Examples/iOS/RatChamber/RatChamberUITests/RatChamberUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class RatChamberUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,7 +18,8 @@ class RatChamberUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        self.app = XCUIApplication()
+        self.app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -26,9 +28,29 @@ class RatChamberUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testFR5() {
+        let button = XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .button).matching(identifier: "Button").element(boundBy: 0)
+        let existsPredicate = NSPredicate(format: "exists == true")
+        expectation(for: existsPredicate,
+                    evaluatedWith: button, handler: nil)
+        waitForExpectations(timeout: 20, handler: nil)
+        XCTAssert(button.exists)
+        XCTAssertTrue(button.isSelected)
+
+        button.tap()
+        XCTAssertTrue(button.isSelected)
+        button.tap()
+        XCTAssertTrue(button.isSelected)
+        button.tap()
+        XCTAssertTrue(button.isSelected)
+        button.tap()
+        XCTAssertTrue(button.isSelected)
+        button.tap()
+        XCTAssertFalse(button.isSelected)
+
+        sleep(6)
+        button.tap()
+        XCTAssertTrue(button.isSelected)
     }
 
 }
