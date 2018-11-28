f0c9606d79a8b9891391a0a45599702e3c91f032
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-11-28 12:47:36 +0900

    :tada: Add map and flatMap funcs in Single #199

---
+++ Sources/Application/Extensions/Single+.swift
@@ -0,0 +1,35 @@
+//
+//  Single+.swift
+//  OperantKit
+//
+//  Created by Yuto Mizutani on 2018/11/28.
+//
+
+import RxSwift
+
+public extension PrimitiveSequence {
+    /**
+     Projects each element of an single sequence into a new form.
+
+     - seealso: [map operator on reactivex.io](http://reactivex.io/documentation/operators/map.html)
+
+     - parameter transform: A transform function to apply to each source element.
+     - returns: An observable sequence whose elements are the result of invoking the transform function on each element of source.
+
+     */
+    public func map<R>(_ transform: @escaping (PrimitiveSequence.E) throws -> R) -> RxSwift.Single<R> {
+        return asObservable().map(transform).asSingle()
+    }
+
+    /**
+     Projects each element of an single sequence to an single sequence and merges the resulting single sequences into one single sequence.
+
+     - seealso: [flatMap operator on reactivex.io](http://reactivex.io/documentation/operators/flatmap.html)
+
+     - parameter selector: A transform function to apply to each element.
+     - returns: An observable sequence whose elements are the result of invoking the one-to-many transform function on each element of the input sequence.
+     */
+    public func flatMap<O>(_ selector: @escaping (PrimitiveSequence.E) throws -> O) -> RxSwift.Single<O.E> where O : ObservableConvertibleType {
+        return asObservable().flatMap(selector).asSingle()
+    }
+}
